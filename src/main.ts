ca066407f244a7ca7cf09eb57ba53ab0b3053796
Vastryx <180879565+Vastryx@users.noreply.github.com>
2026-02-19 18:18:09 -0500

    Add rel attributes to Vite external link

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
+    <a href="https://vite.dev" target="_blank" rel="noreferrer noopener">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
